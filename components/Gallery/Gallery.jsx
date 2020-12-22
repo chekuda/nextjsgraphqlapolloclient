b1b29b510257776f13a13f8cb0d972ca51412085
jose <joselchecaexpo@gmail.com>
2020-12-22 15:51:46 +0000

    create gallery component grid

--- components/Gallery/Gallery.jsx
+++ components/Gallery/Gallery.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
 
-const Gallery = () => {
-  return <div></div>
+import styles from './Gallery.styles'
+
+const Gallery = ({ gallery = [] }) => {
+  const classes = styles()
+  return (
+    <div className={classes.gridContainer}>
+    {
+      gallery.map(data =>
+        <Paper key={data.title} className={classes.paper}>
+          <img src={data.img} className={classes.gridItem} alt={data.title}/>
+        </Paper>
+      )
+    }
+    </div>
+  )
 }
 
 export default Gallery
--- pages/pintura/Pintura.jsx
+++ pages/pintura/Pintura.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Paper from '@material-ui/core/Paper'
+
+import Gallery from 'components/Gallery'
 
 import gallery from 'data/galleryImg/imageList.json'
 
-import styles from './Pintura.styles'
-
-
-const Pintura = () => {
-  const classes = styles()
-  return (
-      <div className={classes.gridContainer}>
-      {
-        gallery.map(data =>
-          <Paper className={classes.paper}>
-            <img src={data.img} className={classes.gridItem} alt={data.title}/>
-          </Paper>
-        )
-      }
-      </div>
-  )
-}
+const Pintura = () => <Gallery gallery={gallery} />
 
 export default Pintura
